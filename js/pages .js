5be03d6846012d793047f07b8bebf74ecf56d2d4
rhytoella0-lab <300094490+rhytoella0-lab@users.noreply.github.com>
2026-08-14 19:24:26 +0900

    Update pages .js

--- js/pages .js
+++ js/pages .js
@@ -1,35 +1,33 @@
 window.pages = {
   
   try: {
-    
     title: "わたしたちの経験から",
-    
     text: "熱量などを書くページです."
-    
   },
   
   things: {
-    
     title: "もののやり方",
-    
     text: "ここに内容を書きます。"
-    
   },
   
   others: {
-    
     title: "もの以外のやり方",
-    
     text: "ここに内容を書きます。"
-    
   },
   
   introduction: {
-    
     title: "紹介ページ",
-    
     text: "ここに紹介内容を書きます。"
-    
+  },
+  
+  beginnerExplanation: {
+    title: "入門版の解説です",
+    text: "ここに入門版の解説内容を書きます。"
+  },
+  
+  advancedExplanation: {
+    title: "応用版の解説です",
+    text: "ここに応用版の解説内容を書きます。"
   }
   
 };
